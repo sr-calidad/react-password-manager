034a7274020200569e3d453ad099635891d1c27c
sr-calidad <sachin.rathod@calidadinfotech.com>
2025-05-27 09:39:29 +0530

    removed the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Navbar/>
+     
       <div className=" bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <Manager/>
       </div>
