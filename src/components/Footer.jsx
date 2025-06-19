8a7949dbf2d394abc0863864f7b62b282040b97b
sr-calidad <sachin.rathod@calidadinfotech.com>
2025-06-19 15:51:13 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Footer = () => {
-  return (
+
     <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
          <div className="logo font-bold text-white text-xl">
             <span className='text-green-500'>&lt;</span>
