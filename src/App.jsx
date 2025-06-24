979e2a8db17f919d9fab7a0a1a9a436f8a48e3f3
sr-calidad <sachin.rathod@calidadinfotech.com>
2025-06-24 14:21:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ import Footer from './components/Footer'
 
 function App() {
 
-  return (
-    <>
+  
      
       <div className=" bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
         <Manager/>
